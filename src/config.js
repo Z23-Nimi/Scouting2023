9f302862fd7d1e26a9bd957d8b5f107d3b9aa642
Z23-Nimi <120608654+Z23-Nimi@users.noreply.github.com>
2022-12-14 18:59:35 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 const config = {
-    firebase_key: "",
-    firebase_auth_domain: "",
-    firebase_project_id: "",
-    firebase_bucket: "",
-    firebase_messagingSenderId: "",
-    firebase_appId: "",
-    firebase_measurementId: "",
+    firebase_key: "AIzaSyCoEodFmGXxTlCV-lRktr_CmrMro2JbwMg",
+    firebase_auth_domain: "scouting-program-858af.firebaseapp.com",
+    firebase_project_id: "scouting-program-858af",
+    firebase_bucket: "project-55014435271.appspot.com",
+    firebase_messagingSenderId: "55014435271",
+    firebase_appId: "1:55014435271:web:1bc69bbc3c2c2a400911e2",
+    firebase_measurementId: "G-YTXBNT707F",
 
-    database_collection: "example-database",
+    database_collection: "scouting-program-858af",
     database_pit_suffix: "-pit"
 }
     
